4570ddf5c115654ffce8b019cee73325fd434141
Paul <ttogom@gmail.com>
2021-01-25 15:04:02 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+// AUTHOR:
+// Description: Hello World in C++
+//===================================================
+
+#include <iostream>
+using namespace std;
+
+int hello1Function(string name1);
+int hello2Function(string name2);
+
+int main() {
+	string name1 = "Paul Jo"; 
+	hello1Function(name1); 
+	string name2 = "your name2"; 
+	hello2Function(name2); 
+	
+	return 0;
+}
