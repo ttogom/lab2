3071b48d94f182ee4455b16c8064ac1d8fc08b15
hknistanaki <47127044+hknistanaki@users.noreply.github.com>
2021-01-25 16:00:44 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int hello2Function(string name2);
 int main() {
 	string name1 = "Paul Jo"; 
 	hello1Function(name1); 
-	string name2 = "your name2"; 
+	string name2 = "Homayoun Nistanaki"; 
 	hello2Function(name2); 
 	
 	return 0;
